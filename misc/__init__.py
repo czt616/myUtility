d1e8329bed07d327977376666226a89a1e20b29c
lukuang <lukuang1989@gmail.com>
2016-03-03 11:50:18 -0500

    add generate index file

--- misc/__init__.py
+++ misc/__init__.py
@@ -1,4 +1,9 @@
 from crawl_url import crawl_url
-from indri_utility import gene_indri_query_file, gene_indri_text_file
+from indri_utility import gene_indri_query_file, gene_indri_text_file,gene_indri_index_para_file
 
-__all__ = ["crawl_url", "gene_indri_query_file", "gene_indri_text_file"]
+__all__ = [
+    "crawl_url", 
+    "gene_indri_query_file", 
+    "gene_indri_text_file",
+    "gene_indri_index_para_file",
+    ]
--- misc/indri_utility.py
+++ misc/indri_utility.py
@@ -36,6 +36,41 @@ def gene_indri_query_file(file_path,queries,index,count,run_id="Infolab"):
         f.write(structure_template.substitute(query_body=query_body,index=index,run_id=run_id,count=str(count)))
 
 
+def gene_indri_index_para_file(corpora,file_path,index_path,memory='2G',stemmer='porter'):
+    index_para_template = Template("""
+    <parameters>
+    <index>$index_path</index>
+    <memory>$memory</memory>
+    $corpora
+    <stemmer><name>$stemmer</name></stemmer>
+    </parameters>
+    """)
+    
+
+    corpus_template = Template("""
+    <corpus>
+      <path>$path</path>
+      <class>trectext</class>
+    </corpus>
+    """)
+    
+    
+    if isinstance(memory,int):
+        memory = str(memory)+'G'
+    if isinstance(memory,str):
+        if memory.isdigit():
+            memory+='G'
+
+
+    corpora = ""
+    for corpus_path in corpora:
+        corpora += corpus_template.substitute(path=corpus_path)
+
+    with codecs.open(file_path, 'w','utf-8') as f:
+        f.write(index_para_template.substitute(index_path=index_path,
+            memory=memory,corpora=corpora,stemmer=stemmer))
+
+
 
 
 def gene_indri_text_file(file_path,texts):
@@ -55,3 +90,4 @@ def gene_indri_text_file(file_path,texts):
         for did in texts:
             text = texts[did].lower()
             f.write(text_template.substitute(did=did,text=text))
+
