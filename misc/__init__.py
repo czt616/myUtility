e2cbfbd982c03d2a1f4c4d57e2f13a2c5aa54e39
lukuang <lukuang1989@gmail.com>
2016-03-03 11:13:58 -0500

    chaneg structure

--- misc/__init__.py
+++ misc/__init__.py
@@ -1,4 +1,4 @@
 from crawl_url import crawl_url
-from indir_utility import gene_indri_query_file, gene_indri_text_file
+from indri_utility import gene_indri_query_file, gene_indri_text_file
 
 __all__ = ["crawl_url", "gene_indri_query_file", "gene_indri_text_file"]
