81fbe4a2fe1c77248f96d6754436af875bceb425
lukuang <lukuang1989@gmail.com>
2016-03-14 19:36:44 -0400

    fix bug

--- corpus/__init__.py
+++ corpus/__init__.py
@@ -1,11 +1,12 @@
 from document import Document
 from text import Text
 from sentence import Sentence
-from model import update_model
+from model import update_model,update_stemmed_model
 
 __all__ = [
     "Document",
     "Sentence",
     "Text",
     "update_model",
+    "update_stemmed_model",
 ]
--- corpus/model.py
+++ corpus/model.py
@@ -3,10 +3,12 @@ model related code
 """
 
 import re
+from myStemmer import pstem as stem 
+from ..misc import do_stem
 
 def update_model(model, text_string=None, text_list=None, text_dict=None):
     if text_string:
-        for w in re.findall("\w+",text_string):
+        for w in re.findall("\w+",text_string.lower()):
             if w not in model:
                 model[w] = 0
             model[w] += 1
@@ -20,6 +22,30 @@ def update_model(model, text_string=None, text_list=None, text_dict=None):
 
     if text_dict:
         for w in text_dict:
+            if w not in model:
+                model[w] = 0
+            model[w] += text_dict[w] 
+
+
+def update_stemmed_model(model, text_string=None, text_list=None, text_dict=None):
+    if text_string:
+        for w in re.findall("\w+",text_string.lower()) :
+            w = stem(w)
+            if w not in model:
+                model[w] = 0
+            model[w] += 1
+
+    if text_list:
+        for w in text_list:
+            w = stem(w)
+            if w not in model:
+                model[w] = 0
+            model[w] += 1
+
+
+    if text_dict:
+        for w in text_dict:
+            w = stem(w)
             if w not in model:
                 model[w] = 0
             model[w] += text_dict[w] 
