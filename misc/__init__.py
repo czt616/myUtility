ac5cfc96640055cd63cf671e64cf3766d5c23993
lukuang <lukuang1989@gmail.com>
2016-06-21 09:53:04 -0400

    add utility to split list for parallel procession

--- misc/__init__.py
+++ misc/__init__.py
@@ -2,6 +2,7 @@ from crawl_url import crawl_url
 from indri_utility import gene_indri_query_file, gene_single_indri_text,gene_indri_text_file,gene_indri_index_para_file
 from other import do_stem,Stopword_Handler
 from my_exception import DebugStop
+from parallel import split_list
 
 __all__ = [
     "crawl_url", 
@@ -11,5 +12,6 @@ __all__ = [
     "gene_indri_index_para_file",
     "do_stem",
     "Stopword_Handler",
-    "DebugStop"
+    "DebugStop",
+    "split_list"
     ]
