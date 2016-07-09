167756ab2c295489043762a040030da875fd3df8
lukuang <lukuang1989@gmail.com>
2016-07-09 19:58:14 -0400

    add IndriQueryFactory class used to generate indri query parameter

--- misc/__init__.py
+++ misc/__init__.py
@@ -1,5 +1,5 @@
 from crawl_url import crawl_url
-from indri_utility import gene_indri_query_file, gene_single_indri_text,gene_indri_text_file,gene_indri_index_para_file
+from indri_utility import gene_indri_query_file, gene_single_indri_text,gene_indri_text_file,gene_indri_index_para_file,IndriQueryFactory
 from other import do_stem,Stopword_Handler,get_stopwords
 from my_exception import DebugStop
 from parallel import split_list
@@ -10,6 +10,7 @@ __all__ = [
     "gene_single_indri_text", 
     "gene_indri_text_file",
     "gene_indri_index_para_file",
+    "IndriQueryFactory",
     "do_stem",
     "Stopword_Handler",
     "DebugStop",
--- misc/indri_utility.py
+++ misc/indri_utility.py
@@ -52,10 +52,111 @@ text_template = Template("""
 \t<TEXT>$text</TEXT>$fields
 </DOC>""")
 
-def gene_indri_query_file(file_path,queries,index,count,run_id="Infolab",rule=None,use_stopper=False,date=None):
+class IndriQueryFactory(object):
+    """Take in query related parameters for indri and
+    generate indri query file
+    """
+    def __init__(self,count,rule=None,
+            use_stopper=False,date_when=None,
+            numeric_compare=None):
+
+        self._count,self._rule,self._use_stopper = 
+                count,rule,use_stopper
+
+        
+
+        if date_when:
+            if date_when not in ["dateafter","datebefore", "datebetween"]:
+                raise ValueError("When value %s is not supported" %(date_when))
+
+        if numeric_compare is not None:
+            if numeric_compare not in ["less","greater","between","equals"]:
+                raise ValueError("Compare value %s is not supported" %(numeric_compare))
+
+
+        self._date_when,self._numeric_compare = 
+                date_when,numeric_compare
+
+
+    def _gene_query(self,file_path,queries,index,
+                date_value=None,numeric_value=None,
+                numeric_field_name=None):
+
+        query_body = ""
+        if self._rule is None:
+            rule = ""
+        else:
+            rule = "<rule>%s</rule>" %self._rule
+
+        if self._use_stopper:
+            stopper = "<stopper>\n"
+            stopwords = get_stopwords()
+            for stopword in stopwords:
+                stopper += "<word>%s</word>\n" %stopword 
+            stopper += "</stopper>"
+        else:
+            stopper = ""
+
+        for qid in queries:
+
+            sinlge_query_data = queries[qid]
+            if isinstance(sinlge_query_data,str):
+                q_string = sinlge_query_data.lower()
+                q_string = "#combine(%s)" %(q_string)
+
+            elif isinstance(sinlge_query_data,list):
+                q_string = " ".join(sinlge_query_data)
+                q_string = "#combine(%s)" %(q_string)
+            
+            elif isinstance(sinlge_query_data,dict):
+                q_string = ""
+                for term in sinlge_query_data:
+                    weight = sinlge_query_data[term]
+                    q_string += "%f %s " %(weight,term)
+
+                q_string = "#weight(%s)" %(q_string)
+            else:
+                raise TypeError("unsupported value type %s for query data" %type(sinlge_query_data))
+
+            q_string = re.sub("[^\w]"," ",q_string)
+            if self._date_when:
+                q_string = "#filrej(#%s(%s) %s)" %(self._date_when,date_value,
+                                                        q_string)
+            
+            if self._numeric_compare is not None:
+                q_string = "#filrej(#%s(%s %d) %s)" %(self._numeric_compare,
+                                            numeric_field_name,numeric_value,q_string)
+
+            query_body+=query_template.substitute(
+                qid=qid,q_string=q_string)
+
+        with codecs.open(file_path, 'w','utf-8') as f:
+            f.write(structure_template.substitute(query_body=query_body,index=index,
+                                                  run_id=run_id,count=str(count),
+                                                  rule=rule,stopper=stopper))
+
+
+    def gene_query_with_date_filter(self,file_path,queries,index,
+                date_value):
+
+        self._gene_query(file_path,queries,index,date_value=date_value)
+
+
+    def gene_query_with_numeric_filter(self,file_path,queries,index,
+            numeric_value,numeric_field_name):
+
+        self._gene_query(file_path,queries,index,,numeric_value=numeric_value,
+                numeric_field_name=numeric_field_name)
+
+
+def gene_indri_query_file(file_path,queries,index,count,
+        run_id="Infolab",rule=None,use_stopper=False,
+        date=None,numeric=None):
 
     """
     generate indri query
+    saved for backward comparability
+    use IndriQueryFactory instead
     """
 
 
@@ -80,10 +181,19 @@ def gene_indri_query_file(file_path,queries,index,count,run_id="Infolab",rule=No
         q_string = re.sub("[^\w]"," ",q_string)
         if date:
             when = date["when"]
-            if when is not in ["dateafter","datebefore", "datebetween"]:
+            if when not in ["dateafter","datebefore", "datebetween"]:
                 raise ValueError("When value %s is not supported" %(when))
+            
             date_value = date["value"]
             q_string = "#filrej(#%s(%s) #combine(%s))" %(when,date_value,q_string)
+        if numeric is not None:
+            compare = numeric["compare"]
+            if compare not in ["less","greater","between","equals"]:
+                raise ValueError("Compare value %s is not supported" %(compare))
+            numeric_field_name = numeric["numeric_field_name"]
+            numeric_value = numeric["value"]
+            q_string = "#filrej(#%s(%s %d) #combine(%s))" %(compare,numeric_field_name,
+                                                numeric_value,q_string)
 
         query_body+=query_template.substitute(
             qid=qid,q_string=q_string)
