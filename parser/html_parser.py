485cc2ceda391b444ee1d6ace49d6c16fee8c7ce
Kuang Lu <lukuang1989@gmail.com>
2017-11-01 15:33:15 -0400

    stop using goose and use Beautifulsoup instead

--- parser/html_parser.py
+++ parser/html_parser.py
@@ -2,9 +2,10 @@
 parse an HTML file to clean text or sentences
 """
 
-from goose import Goose, Configuration
+#from goose import Goose, Configuration
 from ..misc import do_stem
 from nltk.tokenize import sent_tokenize
+from bs4 import BeautifulSoup
 import lxml
 import re
 
@@ -15,10 +16,6 @@ class Html_parser(object):
     use goose to parse raw html and
     """
     def __init__(self,need_stem):
-        #set up goose
-        config = Configuration()
-        config.enable_image_fetching = False
-        self._g = Goose(config)
         self._need_stem = need_stem
 
     def get_text(self,file_path):
@@ -29,16 +26,19 @@ class Html_parser(object):
         if not raw_html:
             return None
         try:
-            article = self._g.extract(raw_html = raw_html)
+            soup = BeautifulSoup(raw_html,"lxml")
+                for script in soup(["script", "style"]):
+                    script.extract()    # rip it out
+            text = re.sub("\s+"," ",soup.get_text())
         except lxml.etree.ParserError as e:
             return None
 
-        text = article.title + ".\n" + article.cleaned_text 
-        if self._need_stem:
-            text = re.sub("\w+",do_stem,text)
-            #words = re.findall("\w+",text,re.MULTILINE)
-            #w = map(stem,words)
-            #text = " ".join(w)
+        else:
+            if self._need_stem:
+                text = re.sub("\w+",do_stem,text)
+                #words = re.findall("\w+",text,re.MULTILINE)
+                #w = map(stem,words)
+                #text = " ".join(w)
         return text
 
     def get_sentences(self,file_path):
